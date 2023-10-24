b27288c2250bad8d369b939fad8e51faa5bbdca7
Marcin Frydrych <marcinf23@gmail.com>
2023-10-25 01:24:34 +0200

    Cos tam cos tam

---
+++ Guess the Flag for iOS 17/Guess_the_Flag_for_iOS_17App.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_the_Flag_for_iOS_17App.swift
+//  Guess the Flag for iOS 17
+//
+//  Created by Marcin Frydrych on 15/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Guess_the_Flag_for_iOS_17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
